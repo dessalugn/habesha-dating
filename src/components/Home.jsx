7215e79a749fd1b20a6122d8519dc143135497cb
Tilahun Dessalegn <dessalugn@gmail.com>
2022-07-14 20:31:13 +0300

    something added

---
+++ src/components/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Home(){
+return<div className="jumbotron">
+    <p>Home </p>
+</div>
+}
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import React from "react";
+import { render } from "react-dom";
+import Home from "./components/Home";
+
+render(<Home/> ,document.getElementById("root"))
